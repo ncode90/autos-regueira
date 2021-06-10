bcc2e53ea5fc7e72b15b4f8d60ba69c742caefe7
ncode90 <nahuel_regueira@hotmail.com>
2021-06-10 15:19:28 -0300

    Desafío: Menú e-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/NavBar.css';
+
+function NavBar(){
+    return(
+        <nav>
+            <div className="logo">Autos Regueira</div>
+        </nav>
+    )
+}
+
+export default NavBar;
